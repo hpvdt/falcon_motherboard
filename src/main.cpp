124b4114ddea65fb8cf3bf25c1a1d8521304703d
z-huu <zach_1003@hotmail.com>
2024-02-01 21:12:20 -0500

    initial test code work

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,41 @@
 #include <Arduino.h>
+#include "ICM42688.h"
+/* Using an Arduino library for the ICM42688 maintained by Inhwan Wee.
+   https://github.com/finani/ICM42688/
+*/
+ICM42688 IMU(SPI, 10); // initializes an ICM chip object, specifying
+                       // protocol + pin select for serial communication
+                       // defaults to 8MHz clock, but a third parameter can be
+                       // passed to specify up to 24 MHz.
 
-const int testPin = PB5;
+// const int testPin = PB5;   from test code
 
-void setup() {
-  pinMode(testPin, OUTPUT);
+void setup()
+{
+  // serial to display data
+  Serial.begin(115200);
+  while (!Serial)
+  {
+  }
+
+  // start communication with IMU
+  Serial.println("initializing...");
+  int status = IMU.begin();
+  if (status < 0)
+  {
+    Serial.println("IMU initialization failed");
+    Serial.print("Status: ");
+    Serial.println(status);
+    while (1)
+    {
+    }
+  }
+
+  Serial.println("good to go");
 }
 
-void loop() {
+void loop()
+{
   digitalWrite(testPin, HIGH);
   delay(2000);
   digitalWrite(testPin, LOW);
