8c5b4e482f800c7c82f1b85427a80f96fc05b64a
EthanTodd8 <ettodd88@gmail.com>
2024-02-01 21:12:03 -0500

    Initial program for DPS310

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,32 @@
 #include <Arduino.h>
+#include <Adafruit_DPS310.h>
 
-const int testPin = PB5;
+Adafruit_DPS310 dps;
+Adafruit_Sensor *dps_pressure = dps.getPressureSensor();
 
 void setup() {
-  pinMode(testPin, OUTPUT);
+  Serial.begin(115200);
+  while (!Serial) delay(10);
+
+  Serial.println("DPS310");
+  if (! dps.begin_I2C()) {
+    Serial.println("Failed to find DPS");
+    while (1) yield();
+  }
+  Serial.println("DPS OK!");
+
+  // Setup highest precision
+  dps.configurePressure(DPS310_64HZ, DPS310_64SAMPLES);
+  dps_pressure->printSensorDetails();
 }
 
 void loop() {
-  digitalWrite(testPin, HIGH);
-  delay(2000);
-  digitalWrite(testPin, LOW);
-  delay(2000);
+  sensors_event_t pressure_event;
+  if (dps.pressureAvailable()) {
+    dps_pressure->getEvent(&pressure_event);
+    Serial.print(F("Pressure = "));
+    Serial.print(pressure_event.pressure);
+    Serial.println(" hPa"); 
+    Serial.println();
+  }
 }
