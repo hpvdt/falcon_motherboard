9a40422fbbcad4bbdb900ee9e70f95f2846b5e98
z-huu <zach_1003@hotmail.com>
2024-02-23 18:18:13 -0500

    code to partially incorporate some LEDs

--- src/main.cpp
+++ src/main.cpp
@@ -4,36 +4,52 @@
    https://github.com/finani/ICM42688/
 */
 
-SPIClass IMUspi(PB15, PB14, PB13);
-ICM42688 IMU(IMUspi, PB10); // initializes an ICM chip object, specifying
-                       // protocol + pin select for serial communication
-                       // defaults to 8MHz clock, but a third parameter can be
-                       // passed to specify up to 24 MHz.
-                       // probably refer to motherboard schematic for specific pin
+SPIClass IMUspi(PB15, PB14, PB13); // Initialize specific SPI bus.
+ICM42688 IMU(IMUspi, PB10);        // initializes an ICM chip object, specifying
+                                   // protocol + pin select for serial communication
+                                   // defaults to 8MHz clock, but a third parameter can be
+                                   // passed to specify up to 24 MHz.
+                                   // PB10 is chip select pin.
 
-void setup() {
-  SerialUSB.begin();
+void setup()
+{
+
+  pinMode(PC8, OUTPUT);   // Top green
+  pinMode(PC9, OUTPUT);   // Middle green
+  pinMode(PB12, OUTPUT);  // Bottom red
+
+  digitalWrite(PC8, HIGH);
+  digitalWrite(PC9, HIGH);
+  digitalWrite(PB12, LOW);
+
+      SerialUSB.begin();
   while (!SerialUSB)
     delay(10); // Wait for USB connection to be made to the computer before continuing
-  
+
   // start communication with IMU
-  SerialUSB.println("initializing...");
+  SerialUSB.println("Initializing...");
   int status = IMU.begin();
 
-  if (status < 0) {
+  if (status < 0)
+  {
     SerialUSB.println("IMU initialization failed");
     SerialUSB.print("Status: ");
     SerialUSB.println(status);
-    while (1) { }
+    while (1)
+    {
+      IMU.begin();
+      digitalWrite(PB12, HIGH); // If initialization failed, red LED turns on
+      delay(10);
+    }
   }
 
   SerialUSB.println("good to go");
 
   delay(20);
-
 }
 
-void loop() {
+void loop()
+{
   IMU.getAGT(); // grab newest readings for accel. and gyro.
   SerialUSB.println("Accelerometer Readings");
   SerialUSB.printf("a_x: %f \t a_y: %f \t a_z: %f \n", IMU.accX(), IMU.accY(), IMU.accZ());
