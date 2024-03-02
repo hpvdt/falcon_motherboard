32c62b961b3691f931ac0367df668209ece1c77d
EthanTodd8 <ettodd88@gmail.com>
2024-03-02 15:38:48 -0500

    Barometer with USB

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,29 @@
 #include <Arduino.h>
 #include <Adafruit_DPS310.h>
+#include <USBSerial.h>
+#include <Wire.h>
+
 
 Adafruit_DPS310 dps;
 Adafruit_Sensor *dps_pressure = dps.getPressureSensor();
+HardwareSerial Serial1(PA3, PA2);
+const int LEDPIN = PC8;
+const int LEDPIN2 = PC9;
 
 void setup() {
-  Serial.begin(115200);
-  while (!Serial) delay(10);
+  SerialUSB.begin();
+  SerialUSB.begin(9600);
+  while (!SerialUSB) delay(10); 
+  pinMode(LEDPIN, OUTPUT);
+  pinMode(LEDPIN2, OUTPUT);
 
-  Serial.println("DPS310");
+  SerialUSB.println("DPS310");
+
   if (! dps.begin_I2C()) {
-    Serial.println("Failed to find DPS");
+    SerialUSB.println("Failed to find DPS");
     while (1) yield();
   }
-  Serial.println("DPS OK!");
+  SerialUSB.println("DPS OK!");
 
   // Setup highest precision
   dps.configurePressure(DPS310_64HZ, DPS310_64SAMPLES);
@@ -21,12 +31,15 @@ void setup() {
 }
 
 void loop() {
+  digitalWrite(LEDPIN, HIGH);
+  digitalWrite(LEDPIN2, HIGH);
+  delay(1000);
   sensors_event_t pressure_event;
   if (dps.pressureAvailable()) {
     dps_pressure->getEvent(&pressure_event);
-    Serial.print(F("Pressure = "));
-    Serial.print(pressure_event.pressure);
-    Serial.println(" hPa"); 
-    Serial.println();
+    SerialUSB.print(F("Pressure = "));
+    SerialUSB.print(pressure_event.pressure);
+    SerialUSB.println(" hPa"); 
+    SerialUSB.println();
   }
 }
