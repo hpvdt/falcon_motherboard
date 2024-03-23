89b67e1f872a865838893f96de4c3fe32d309728
EthanTodd8 <ettodd88@gmail.com>
2024-03-23 18:08:46 -0400

    Workig Lidar

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,144 @@
-#include <Arduino.h>
-#include <Adafruit_DPS310.h>
-#include <USBSerial.h>
-#include <Wire.h>
+/* File Name: TFMPI2C_example.ino
+ * Inception: 29 JAN 2019
+ * Last work: 19 MAY 2021
+ * Developer: Bud Ryerson
+ *
+ * Description: This Arduino sketch is used to test the Benewake
+ * TFMini-Plus time-of-flight Lidar ranging sensor in the I2C
+ * communication interface mode with the default I2C address
+ * using the TFMPI2C library.
+ *
+ * Default settings for the TFMini-Plus I2C are:
+ *    0x10  -  slave device address
+ *    100Mhz - bus clock speed
+ *    100Hz  - data frame-rate
+ *    Centimeter - distance measurement format
+ *    Celsius - temperature measurement scale
+ *
+ * The TFMini-Plus is switched from the default UART (serial)
+ * interface mode to I2C Mode by a command from the TFMPlus library
+ * or the factory supplied GUI.  The device will remain in I2C Mode
+ * regardless of any reset commands or power cycling until the I2C
+ * command 'SET_SERIAL_MODE' is sent.
+ *
+ * NOTE: If your your Arduino is capable of operating at "Fast" I2C
+ * clock speeds (400KHz), then remove comment slashes from line #75.
+ *
+ * There are only two important functions: 'getData' and 'sendCommand'
+ *
+ *  NOTE: By default the I2C device address is set to 0x10. If you need
+ *  to address multiple devices or need to change the default address for
+ *  any reason, your code must thereafter include an unsigned, 8-bit
+ *  'addr' value at the end of every call to 'getData()' or 'sendCommand()'.
+ *
+ *  'getData( dist, flux, temp, addr)' passes back measurement values in
+ *  three signed, 16-bit variables:
+ *     dist - distance to target in centimeters: 10cm - 1200cm
+ *     flux - strength, voltage or quality of returned signal
+ *            in arbitrary units: -1, 0 - 32767
+ *     temp - chip temperature in Celsius: -25°C to 125°C
+ *  and sends...
+ *     addr - an optional unsigned 8-bit address value.
+ *
+ *  - If the default device address is used, the 'addr' value may be
+ *  mitted.  Otherwise, a correct 'addr' value always must be sent.
+ *  - If a function completes without error, it returns 'True' and sets
+ *  a public, one-byte 'status' code to zero ('READY').  Otherwise, it
+ *  returns 'False' and sets the 'status' code to a library defined error.
+ *  
+ *  NOTE: 'getData( dist)' is a simplified function that passes back
+ *  distance data only; but assumes use of the default I2C address.
+ *
+ * 'sendCommand( cmnd, param, addr)'
+ *  The function sends an unsigned 32-bit command and an unsigned 32-bit
+ *  parameter value plus an optional, unsigned, 8-bit I2C device address.
+ *  If the function completes without error it returns 'True' and sets
+ *  a public one-byte 'status' code to zero.  Otherwise it returns 'False'
+ *  and sets the 'status' code to a library defined error code.
+ *
+ *  NOTE: The 'cmmd' value must be chosen from the library's list of defined
+ *  commands. Parameters can be entered directly (0x10, 250, etc.) or chosen
+ *  from the library's lists of defined parameters.
+ */
 
+#include <Wire.h>     // Arduino standard I2C/Two-Wire Library
 
-Adafruit_DPS310 dps;
-Adafruit_Sensor *dps_pressure = dps.getPressureSensor();
-TwoWire mainbus(PB7, PB6);
+                      // devices such as the Galileo. Download from:
+                      // https://github.com/spaniakos/AES/blob/master/printf.h
 
-const int LEDPIN = PC8;
-const int LEDPIN2 = PC9;
+#include <TFMPI2C.h>  // TFMini-Plus I2C Library v1.6.0
+TwoWire tfBus(PB7, PB6);
+TFMPI2C tfmP(&tfBus);         // Create a TFMini-Plus I2C object
 
-void setup() {
-  SerialUSB.begin();
-  SerialUSB.begin(9600);
-  while (!SerialUSB) delay(10); 
-  pinMode(LEDPIN, OUTPUT);
-  pinMode(LEDPIN2, OUTPUT);
+void setup(){
+    SerialUSB.begin( 115200);   // Initialize terminal serial port
+    delay(20);
 
-  SerialUSB.println("DPS310");
+    tfBus.begin();            // Initialize two-wire interface
+//    tfBus.setClock( 400000);  // Set I2C bus speed to 'Fast' if
+                               // your Arduino supports 400KHz.
 
-  if (! dps.begin_I2C(119, &mainbus)) {
-    SerialUSB.println("Failed to find DPS");
-    while (1) yield();
-  }
-  SerialUSB.println("DPS OK!");
+    SerialUSB.print( "\r\nTFMPlus I2C Library 1.6.0\r\n");  // say 'hello'
 
-  // Setup highest precision
-  dps.configurePressure(DPS310_64HZ, DPS310_64SAMPLES);
-  dps_pressure->printSensorDetails();
+    // Send some example commands to the TFMini-Plus
+    // - - Perform a system reset - - - - - - - - - - -
+    // SerialUSB.print( "System reset: ");
+    // if( tfmP.sendCommand( SOFT_RESET, 0))
+    // {
+    //     SerialUSB.print( "passed");
+    // }
+    // else tfmP.printReply();
+    // - - Display the firmware version - - - - - - - - -
+    // printf( "Firmware version: ");
+    // if( tfmP.sendCommand( GET_FIRMWARE_VERSION, 0))
+    // {
+    //     printf( "%1u.", tfmP.version[ 0]); // print three single numbers
+    //     printf( "%1u.", tfmP.version[ 1]); // each separated by a dot
+    //     printf( "%1u\r\n", tfmP.version[ 2]);
+    // }
+    // else tfmP.printReply();
+    // - - Set the data frame-rate to 20 - - - - - - - - -
+//     printf( "Data-Frame rate: ");
+//     if( tfmP.sendCommand( SET_FRAME_RATE, FRAME_20))
+//     {
+//         printf( "%2uHz.\r\n", FRAME_20);
+//     }
+//     else tfmP.printReply();
+//     // - - - - - - - - - - - - - - - - - - - - - - - -
+// /*    // - - Set Serial Mode - - - - - - - - - - -
+//     printf( "Set Serial Mode: ");
+//     if( tfmP.sendCommand( SET_SERIAL_MODE, 0))
+//     {
+//         printf( "passed.\r\n");
+//     }
+//     else tfmP.printReply();
+//     // - - Set Serial Mode - - - - - - - - - - -
+//     printf( "Save Settring: ");
+//     if( tfmP.sendCommand( SAVE_SETTINGS, 0))
+//     {
+//         printf( "passed.\r\n");
+//     }
+//     else tfmP.printReply();
+// */
+//     delay(500);            // And wait for half a second.
 }
 
-void loop() {
-  digitalWrite(LEDPIN, HIGH);
-  digitalWrite(LEDPIN2, HIGH);
-  delay(1000);
-  sensors_event_t pressure_event;
-  if (dps.pressureAvailable()) {
-    dps_pressure->getEvent(&pressure_event);
-    SerialUSB.print(F("Pressure = "));
-    SerialUSB.print(pressure_event.pressure);
-    SerialUSB.println(" hPa"); 
-    SerialUSB.println();
-  }
+// Initialize data variables
+int16_t tfDist = 0;       // Distance to object in centimeters
+int16_t tfFlux = 0;       // Signal strength or quality of return signal
+int16_t tfTemp = 0;       // Internal temperature of Lidar sensor chip
+
+// = = = = = = = = = =  MAIN LOOP  = = = = = = = = = =
+void loop(){
+    tfmP.getData( tfDist, tfFlux, tfTemp); // Get a frame of data
+    if( tfmP.status == TFMP_READY){
+      SerialUSB.print(tfmP.status);
+      SerialUSB.print(tfDist);   // display distance,
+      SerialUSB.println(tfTemp);   // display signal strength/quality,[0]
+        // printf( "Temp:%2i%s",  tfTemp, "°C" );   // display temperature,
+        // printf( "\r\n");                   // end-of-line.
+    }
+    SerialUSB.println(tfmP.status);
+    delay(100);    //  Run loop at approximately 20Hz.
 }
+// = = = = = = = = =  End of Main Loop  = = = = = = = = =
