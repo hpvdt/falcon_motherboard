1df8b8fce5ce686d392dd4fc41d46ff219c7ba44
z-huu <zach_1003@hotmail.com>
2024-03-23 17:46:28 -0400

    IMU wrapper functions done

--- lib/IMU/6-Axis-IMU.cpp
+++
@@ -1,9 +0,0 @@
-#include "6-Axis-IMU.h"
-
-void IMU_setup(ICM42688 IMU) {
-    // hi
-}
-
-void IMU_measurements(ICM42688 IMU) {
-    // hi
-}
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,11 @@
-#include <Arduino.h>
-#include "ICM42688.h"
+#include "6-Axis-IMU.h"
 /* Using an Arduino library for the ICM42688 maintained by Inhwan Wee.
    https://github.com/finani/ICM42688/
 */
 
-SPIClass IMUspi(PB15, PB14, PB13); // Initialize specific SPI bus.
-ICM42688 IMU(IMUspi, PB10);        // initializes an ICM chip object, specifying
-                                   // protocol + pin select for serial communication
-                                   // defaults to 8MHz clock, but a third parameter can be
-                                   // passed to specify up to 24 MHz.
-                                   // PB10 is chip select pin.
-
 void setup()
 {
-
+  /*    some debugging LED code
   pinMode(PC8, OUTPUT);   // Top green
   pinMode(PC9, OUTPUT);   // Middle green
   pinMode(PB12, OUTPUT);  // Bottom red
@@ -21,52 +13,19 @@ void setup()
   digitalWrite(PC8, HIGH);
   digitalWrite(PC9, HIGH);
   digitalWrite(PB12, LOW);
+  */
+  
+  while (!SerialUSB) delay(10); 
+  // Wait for USB connection to be made to the computer before continuing
 
-      SerialUSB.begin();
-  while (!SerialUSB)
-    delay(10); // Wait for USB connection to be made to the computer before continuing
+  IMU_setup();
 
-  // start communication with IMU
-  SerialUSB.println("Initializing...");
-  int status = IMU.begin();
-
-  if (status < 0)
-  {
-    SerialUSB.println("IMU initialization failed");
-    SerialUSB.print("Status: ");
-    SerialUSB.println(status);
-    while (1)
-    {
-      IMU.begin();
-      digitalWrite(PB12, HIGH); // If initialization failed, red LED turns on
-      delay(10);
-    }
-  }
-
-  SerialUSB.println("good to go");
-  digitalWrite(PB12, LOW);    // Red LED goes off, good to go
-  delay(20);
+  delay(10);
+  SerialUSB.begin();
 }
 
 void loop()
 {
-  IMU.getAGT(); // grab newest readings for accel. and gyro.
-  SerialUSB.println("Accelerometer Readings");
-  //SerialUSB.printf("a_x: %f \t a_y: %f \t a_z: %f \n", IMU.accX(), IMU.accY(), IMU.accZ());
-  SerialUSB.println("a_x: ");
-  SerialUSB.print(IMU.accX());
-  SerialUSB.println("a_y: ");
-  SerialUSB.print(IMU.accY());
-  SerialUSB.println("a_z: ");
-  SerialUSB.print(IMU.accZ());
-  SerialUSB.println("Gyroscope Readings");
-  SerialUSB.print("g_x: ");
-  SerialUSB.println(IMU.gyrX());
-  SerialUSB.print("g_y: ");
-  SerialUSB.println(IMU.gyrY());
-  SerialUSB.print("g_z: ");
-  SerialUSB.println(IMU.gyrZ());
-  SerialUSB.print("Temperature: ");
-  SerialUSB.println(IMU.temp());
-  delay(100);
+  IMU_measurements();
+  delay(1000);
 }
