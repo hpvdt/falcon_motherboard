fd4519fbafdb4e535a3772b45872478611303ff5
z-huu <zach_1003@hotmail.com>
2024-03-26 21:32:56 -0400

    start of wrapper functions

---
+++ lib/sensors/BNO.cpp
@@ -0,0 +1,3 @@
+#include "BNO.h"
+
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28, &Wire);
---
+++ lib/sensors/BNO.h
@@ -0,0 +1,9 @@
+
+#ifndef BNO_HEADER
+#define BNO_HEADER
+#include <Adafruit_BNO055.h>
+
+void setupBNO();
+void measureBNO();
+void printBNO();
+#endif
