9e360992cc3a67269c7f5d084819329851c0b581
EthanTodd8 <ettodd88@gmail.com>
2024-06-04 21:34:02 -0400

    Simplified for merge

--- src/main.cpp
+++ src/main.cpp
@@ -9,39 +9,21 @@
     PB4 - Clock to HX711
     PB5 - RESET
 */
-const int owAddMain = 2;
+const int owAdd = 2;
 const int owAddTest = 0b1010; // Hardcoded address for spar boards.
-const int LEDPIN = PC8;
 const int owTX  = PC3;
 const int owRX  = PC2;
-const int owAdd = owAddMain;
 
 
 void setup() {
-  pinMode(LEDPIN, OUTPUT);
   setupOneWire(owRX, owTX, owAdd, false);
   Serial.begin(115200);
-  delay(100);
-  Serial.println("RUNNING THE TEST STATION CODE");
-  Serial.println("MISSES / TESTS");
 
   int32_t rec;
   requestOneWire(owAddTest, &rec);
-  Serial.println(rec);
 }
 
 void loop() {
-  digitalWrite(LEDPIN, HIGH);
-  static unsigned int count = 0;
-  static unsigned int missed = 0;
-
   int32_t rec = 0;
-  if (requestOneWire(owAddTest, &rec) == false) missed++;
-  count++;
-
-  // Serial.print(missed);
-  // Serial.print("/");
-  // Serial.println(count);
-  Serial.println(rec);
-  delay(5);
+  requestOneWire(owAddTest, &rec);
 }
