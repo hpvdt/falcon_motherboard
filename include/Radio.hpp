4b99d0e29bac41a4dfecd4f0df37e4e398dde677
Savo Bajic <bajic.savo@gmail.com>
2024-07-10 01:10:52 -0400

    Starting radio code
    Code is being reworked to keep most of radio work out of `main.cpp`.
    Reworked so references are used instead of pass-by-copy to make code
    easier to reuse safely.
    
    Also added constants for radios based on rev 3 schematic.

--- include/Radio.hpp
+++ include/Radio.hpp
@@ -2,8 +2,14 @@
 #define RADIO_HEADER_H
 
 #include <Arduino.h>
+#include <SPI.h>
+#include <RF24.h>
 
-bool setupRadio(SPIClass* radio_bus, uint8_t CE_PIN, uint8_t CSN_PIN, uint8_t level);
+int setupRadios(SPIClass* radio_bus);
 
-
+int defaultSetupRadio(SPIClass* radio_bus, RF24* radio);
+void setReceiver(RF24* radio, uint8_t pipe, uint64_t address);
+void setTransmitter(RF24* radio, uint64_t address);
+void radioRecieved(RF24* radio);
+bool radioSend(RF24* radio, const char* outputMessage, byte messageLen);
 #endif
--- src/Radio.cpp
+++ src/Radio.cpp
@@ -3,45 +3,65 @@
 #include <SPI.h>
 #include <RF24.h>
 
+const uint8_t INTERNAL_CE_PIN = PB5;
+const uint8_t INTERNAL_CSN_PIN = PB9;
+const uint8_t INTERNAL_IRQ_PIN = PB4;
+RF24 int_radio(INTERNAL_CE_PIN, INTERNAL_CSN_PIN); // Radio used for internal communications
+
+const uint8_t EXTERNAL_CE_PIN = PA9;
+const uint8_t EXTERNAL_CSN_PIN = PA8;
+const uint8_t EXTERNAL_IRQ_PIN = PA10;
+RF24 ext_radio(EXTERNAL_CE_PIN, EXTERNAL_CSN_PIN); // Radio used for external communications
+
 byte pipeNumber;
 char radioMessage[32];
 volatile bool recievedRadioData = false; 
 
-int setupRadio(SPIClass* radio_bus, uint8_t CE_PIN, uint8_t CSN_PIN){
-    RF24 radio(CE_PIN, CSN_PIN);
-    if (!radio.begin(radio_bus)) {
-        SerialUSB.println(F("radio hardware is not responding!!"));
-        while (1) {}  // hold in infinite loop
+int setupRadios(SPIClass* radio_bus) {
+    defaultSetupRadio(radio_bus, &int_radio);
+    defaultSetupRadio(radio_bus, &ext_radio);
+
+    return 0; // A-okay radio start
+}
+
+int defaultSetupRadio(SPIClass* radio_bus, RF24* radio) {
+    
+    if (!radio->begin(radio_bus)) {
+        // SerialUSB.println(F("radio hardware is not responding!!"));
+        // while (1) {}  // hold in infinite loop
+        return -1; // Failed to start radio
     }
-    radio.setAutoAck(true);
-    radio.setPALevel(RF24_PA_MAX);
-    radio.setDataRate(RF24_250KBPS);
-    radio.enableAckPayload();               // Allow optional ack payloads
-    radio.setRetries(5, 15);                // Smallest time between retries, max no. of retries
-    radio.enableDynamicPayloads();
-    radio.setChannel(100);
+    radio->setAutoAck(true);
+    radio->setPALevel(RF24_PA_MAX);
+    radio->enableAckPayload();               // Allow optional ack payloads
+    radio->setRetries(5, 15);                // Smallest time between retries, max no. of retries
+    radio->enableDynamicPayloads();
+    radio->setChannel(100);
+    if(radio->setDataRate(RF24_250KBPS) != true) return -2; // Failed to set radio data rate
+    
+    return 0; // Radio setup sans issue
 }
 
-void setReceiver(RF24 radio, uint8_t pipe, uint8_t address){
-    radio.openReadingPipe(pipe, address);    
-    radio.startListening();
+void setReceiver(RF24* radio, uint8_t pipe, uint64_t address) {
+    radio->openReadingPipe(pipe, address);    
+    radio->startListening();
 }
 
-void setTransmitter(RF24 radio, uint8_t address){
-    radio.openWritingPipe(address);
-    radio.stopListening();
+void setTransmitter(RF24* radio, uint64_t address) {
+    radio->openWritingPipe(address);
+    radio->stopListening();
 }
 
-void radioRecieved(RF24 radio) {
+void radioRecieved(RF24* radio) {
   recievedRadioData = true;
 
   // Start by getting where the message is from
-  radio.available(&pipeNumber); // Get pipe the message was recieved on in the event a reply is needed
+  radio->available(&pipeNumber); // Get pipe the message was recieved on in the event a reply is needed
 
   // Read the message. Reads both messages and acknoledgements. 
-  radio.read(&radioMessage, radio.getDynamicPayloadSize());
+  radio->read(&radioMessage, radio->getDynamicPayloadSize());
 }
 
-void radioSend(const char* outputMessage, byte messageLen) {
-    
+bool radioSend(RF24* radio, const char* outputMessage, byte messageLen) {
+    return radio->write(outputMessage, messageLen);
 }
