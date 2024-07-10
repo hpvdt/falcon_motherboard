7ee24b6e73b746ba42af3d5e60b7e837a32f2bb7
Savo Bajic <bajic.savo@gmail.com>
2024-07-10 01:39:13 -0400

    Whitespacing to 4 spaces

--- lib/sensors/GPS.cpp
+++ lib/sensors/GPS.cpp
@@ -12,19 +12,19 @@ void GPSSetup() {
     gps_serial.begin(GPS_BAUDRATE);
 }
 
-void getGPSdata(float *latitude, float *longitude, float *speedGPS, float *altitude){
+void getGPSdata(float *latitude, float *longitude, float *speedGPS, float *altitude) {
     bool newData = false;
-  while (gps_serial.available()) {
-    char c = gps_serial.read();
-    if (gps.encode(c)) newData = true; // Did a new valid sentence come in?
-  }
-  if (newData) {
-    unsigned long age;
-    gps.f_get_position(latitude, longitude, &age);
-    *latitude = (*latitude == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : *latitude);
-    *longitude = (*longitude == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : *longitude);
-    *altitude = gps.f_altitude();
-    *speedGPS = gps.f_speed_kmph();
-  }
+    while (gps_serial.available()) {
+        char c = gps_serial.read();
+        if (gps.encode(c)) newData = true; // Did a new valid sentence come in?
+    }
+    if (newData) {
+        unsigned long age;
+        gps.f_get_position(latitude, longitude, &age);
+        *latitude = (*latitude == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : *latitude);
+        *longitude = (*longitude == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : *longitude);
+        *altitude = gps.f_altitude();
+        *speedGPS = gps.f_speed_kmph();
+    }
 }
     
