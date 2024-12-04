440f1bfc00fa2fd4487c2fcf39253fb8fc2e02b2
Savo Bajic <bajic.savo@gmail.com>
2024-12-04 02:49:59 -0500

    Reduced OneWire bit period from 65 to 55us

--- lib/onewire/onewireConfig.cpp
+++ lib/onewire/onewireConfig.cpp
@@ -1,7 +1,7 @@
 #include <stdint.h>
 #include "onewireConfig.hpp"
 
-const uint8_t OW_BIT_PERIOD = 65;       // Period for each bit in us (needs to be at least thrice `OW_PULSE_PERIOD`)
+const uint8_t OW_BIT_PERIOD = 55;       // Period for each bit in us (needs to be at least thrice `OW_PULSE_PERIOD`)
 const uint8_t OW_PULSE_PERIOD = 15;     // Minimum period the pulse is held for a bit on one wire bus
 const uint8_t OW_ADDRESS_WIDTH = 4;     // Number of bits for device addresses on one wire bus
 const uint8_t OW_DATA_WIDTH = 24;       // Width of data response on one wire bus
