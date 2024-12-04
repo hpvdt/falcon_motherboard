66bfef653ae8e1339ee95c0aa083fadfeceecce2
Savo Bajic <bajic.savo@gmail.com>
2024-12-04 02:45:55 -0500

    Added comments for communications functions

--- include/communications.hpp
+++ include/communications.hpp
@@ -20,5 +20,23 @@ enum CommunicationChannel {
  */
 void setup_pc_comms(bool block);
 
+/**
+ * \brief Broadcast a formatted message about the aircrtaft's state on a channel
+ * 
+ * \param msg_type Message type to send
+ * \param chn Communications channel to boardcast it on
+ * \param state Aircraft state data to use
+ * 
+ * \return Any error code for transmissions (`0` for OK)
+ */
 int send_mesage(MessageType msg_type, CommunicationChannel chn, AircraftState* state);
+
+/**
+ * \brief Broadcast a test formatted message about the aircrtaft's state on a channel
+ * 
+ * \param msg_type Message type to send
+ * \param chn Communications channel to boardcast it on
+ * 
+ * \return Any error code for transmissions (`0` for OK)
+ */
 int send_test_mesage(MessageType msg_type, CommunicationChannel chn);
